f5e88c7c98f78bb4da0ee872445b3a8c1b7bb5fb
kohama_daichi <k016c1369@it-neec.jp>
2017-10-24 07:56:43 +0900

    AndroidManifest.xmlからアクションバーを隠すようにした

--- app/src/main/java/com/tetsujin/tt/MainActivity.java
+++ app/src/main/java/com/tetsujin/tt/MainActivity.java
@@ -33,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
         //日付取得と表示
