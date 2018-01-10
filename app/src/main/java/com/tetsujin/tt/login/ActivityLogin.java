8225bdd71dc2bc7be0d8048e723fc3ad1b504238
小濵大地 <k016c1369@it-neec.jp>
2018-01-10 22:47:51 +0900

    ログイン機能実装の続き

--- app/src/main/java/com/tetsujin/tt/login/ActivityLogin.java
+++ app/src/main/java/com/tetsujin/tt/login/ActivityLogin.java
@@ -29,7 +29,7 @@ public class ActivityLogin extends AppCompatActivity
         
         final ActivityLogin activityLogin = this;
         final FirebaseAuth auth = FirebaseAuth.getInstance();
-        
+
         try
         {
             mClient = new MobileServiceClient("https://tetsujin-tt.azurewebsites.net", this);
@@ -45,18 +45,18 @@ public class ActivityLogin extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                EditText studentNum = (EditText)findViewById(R.id.AyLogin_studentnum_edittext);
+                EditText studentId = (EditText)findViewById(R.id.AyLogin_studentid_edittext);
                 EditText password = (EditText)findViewById(R.id.AyLogin_password_edittext);
                 final TextView notice = (TextView) findViewById(R.id.AyLogin_notice_textview);
-                
+
                 //入力チェック
-                if(studentNum.getText().toString().isEmpty() &&
+                if(studentId.getText().toString().isEmpty() &&
                         password.getText().toString().isEmpty())
                 {
                     notice.setText(R.string.forget_input_all);
                     return;
                 }
-                else if(studentNum.getText().toString().isEmpty())
+                else if(studentId.getText().toString().isEmpty())
                 {
                     notice.setText(R.string.forget_input_number);
                     return;
@@ -66,37 +66,35 @@ public class ActivityLogin extends AppCompatActivity
                     notice.setText(R.string.forget_input_password);
                     return;
                 }
-                
+
                 //入力欄が空ではないならログイン処理を実行
                 final ProgressDialog progressDialog = new ProgressDialog(activityLogin);
                 progressDialog.setCancelable(false);
                 progressDialog.setMessage(getResources().getString(R.string.logging_in));
                 progressDialog.show();
-    
-                AsyncTask<Void, Void, Void> getEmailTask = new AsyncTask<Void, Void, Void>()
-                {
-                    @Override
-                    protected Void doInBackground(Void... voids)
-                    {
-                        MobileServiceTable<> table = mClient.getTable();
-                        
-                    }
-                };
-                
+
+//                AsyncTask<Void, Void, Void> getEmailTask = new AsyncTask<Void, Void, Void>()
+//                {
+//                    @Override
+//                    protected Void doInBackground(Void... voids)
+//                    {
+//                        //MobileServiceTable<> table = mClient.getTable()
+//                    };
+//                };
+
                 //ログイン実行
-                auth.signInWithEmailAndPassword(studentNum.getText().toString(), password.getText().toString())
+                auth.signInWithEmailAndPassword(studentId.getText().toString(), password.getText().toString())
                         .addOnCompleteListener(activityLogin, new OnCompleteListener<AuthResult>()
                         {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task)
                             {
                                 progressDialog.dismiss();
-                                
+
                                 //ログイン成功時
                                 if(task.isSuccessful())
                                 {
                                     Toast.makeText(activityLogin, "Success", Toast.LENGTH_SHORT).show();
-                                    
                                 }
                                 //失敗時
                                 else
@@ -106,6 +104,16 @@ public class ActivityLogin extends AppCompatActivity
                                 }
                             }
                         });
+            }
+        });
+
+        //新規登録ボタンのクリックイベント
+        findViewById(R.id.AyLogin_signup_button).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
             }
         });
     }
